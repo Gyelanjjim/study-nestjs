59dc9e01f48a5643c4944b146728dd10575b0f9e
songcj0720 <songcj92@gmail.com>
2025-07-01 14:22:22 +0900

    add: 아키텍처 controller - service

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -7,111 +7,37 @@ import {
   Param,
   Patch,
   Post,
+  Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-/**
- * author
- * title
- * content
- * likeCount
- * connectionCount
- * images
- */
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'noname',
-    title: '제목없음',
-    content: '냉무',
-    likeCount: 0,
-    commentCount: 0,
-  },
-  {
-    id: 2,
-    author: 'noname',
-    title: '제목없음2',
-    content: '냉무',
-    likeCount: 1,
-    commentCount: 2,
-  },
-  {
-    id: 3,
-    author: 'noname',
-    title: '제목없음3',
-    content: '냉무',
-    likeCount: 3,
-    commentCount: 1,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  /**
-   * @desc GET /posts
-   * @returns
-   */
   @Get()
-  getPost(): PostModel[] {
-    return posts;
+  getPost() {
+    return this.postsService.getAllPosts();
   }
 
   @Get(':id')
-  getPostById(@Param('id') id: string, @Param('name') name: string) {
+  getPostById(@Param('id') id: string) {
     // path parameter 에서 id 를 가져와서 id 라는 변수에 string 으로 저장한다
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-      // {
-      //   "message": "Not Found",
-      //   "statusCode": 404
-      // }
-    }
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   @Get(':name/:title')
   searchPost(@Param('name') name: string, @Param('title') title: string) {
-    const post = posts.find(
-      (post) => post.title.startsWith(title) && post.author === name,
-    );
-    if (!post) {
-      throw new NotFoundException();
-    }
-    return post;
+    return this.postsService.searchPost(title, name);
   }
 
-  /**
-   * @desc POST /posts
-   */
   @Post()
   createPost(
     @Body('author') author: string,
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0, // 초기화
-      commentCount: 0, // 초기화
-    };
-    posts = [...posts, post];
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   @Patch(':id')
@@ -121,25 +47,11 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-    if (author) post.author = author;
-    if (title) post.title = title;
-    if (content) post.content = content;
-
-    posts = posts.map((v) => (v.id === +id ? post : v));
-    return post;
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-    posts = posts.filter((v) => v.id !== +id);
-    return id;
+    return this.postsService.deletePost(id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,115 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+/**
+ * author
+ * title
+ * content
+ * likeCount
+ * connectionCount
+ * images
+ */
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'noname',
+    title: '제목없음',
+    content: '냉무',
+    likeCount: 0,
+    commentCount: 0,
+  },
+  {
+    id: 2,
+    author: 'noname',
+    title: '제목없음2',
+    content: '냉무',
+    likeCount: 1,
+    commentCount: 2,
+  },
+  {
+    id: 3,
+    author: 'noname',
+    title: '제목없음3',
+    content: '냉무',
+    likeCount: 3,
+    commentCount: 1,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+      // {
+      //   "message": "Not Found",
+      //   "statusCode": 404
+      // }
+    }
+    return post;
+  }
+
+  searchPost(title: string, name: string) {
+    const post = posts.filter(
+      (post) => post.title.startsWith(title) && post.author === name,
+    );
+    if (post.length <= 0) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0, // 초기화
+      commentCount: 0, // 초기화
+    };
+    posts = [...posts, post];
+    return post;
+  }
+
+  updatePost(
+    postId: number,
+    author?: string,
+    title?: string,
+    content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +postId);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    if (author) post.author = author;
+    if (title) post.title = title;
+    if (content) post.content = content;
+
+    posts = posts.map((v) => (v.id === +postId ? post : v));
+    return post;
+  }
+
+  deletePost(id: string) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    posts = posts.filter((v) => v.id !== +id);
+    return id;
+  }
+}
